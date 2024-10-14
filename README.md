ac2c2fcba51a28ff4b20fa52de58150a52586020
Sreshth <sreshthtiwari13@gmail.com>
2024-10-14 13:17:58 -0400

    add files

---
+++ README.md
@@ -0,0 +1,1 @@
+# Repo for CS1 Assignment
---
+++ main.py
@@ -0,0 +1,11 @@
+from cs1lib import *
+
+screen_height = 900
+screen_width = 900
+
+def main_draw():
+    clear()
+
+    set_clear_color(0,0,0)
+
+start_graphics(main_draw, width=screen_width, height=screen_height)
