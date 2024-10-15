8006fa6540c4484228f97faab35e23a1a9fb22a1
Sreshth <sreshthtiwari13@gmail.com>
2024-10-14 20:44:37 -0400

    checkpoint 1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Repo for CS1 Assignment
+
+issues:
+
+1. can't simultaneously move paddles
--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+'''
+Author
+Date
+Purpose
+'''
 from cs1lib import *
+
+paddle_movement_height = 10
 
 screen_height = 400
 screen_width = 400
@@ -34,47 +41,52 @@ def draw_paddles():
     draw_rectangle(right_paddle_x, right_paddle_y, paddle_width, paddle_height)    
 
 def move_paddles():
-    global left_down, left_up, right_down, right_up, left_paddle_y, right_paddle_y, paddle_height
+    global left_down, left_up, right_down, right_up, left_paddle_y, right_paddle_y, paddle_height, paddle_movement_height
 
     if left_up and left_paddle_y>=0:
-        left_paddle_y-=5
-        left_up = False
+        left_paddle_y-=paddle_movement_height
 
-    if left_down and left_paddle_y<=screen_height-paddle_height:
-        left_paddle_y+=5
-        left_down = False
+    if left_down and left_paddle_y<=screen_height-paddle_height-paddle_movement_height:
+        left_paddle_y+=paddle_movement_height
 
     if right_up and right_paddle_y>=0:
-        right_paddle_y-=5
-        right_up = False
+        right_paddle_y-=paddle_movement_height
 
-    if right_down and right_paddle_y<=screen_height-paddle_height:
-        right_paddle_y+=5
-        print(right_paddle_y, screen_height)    
-        right_down = False
+    if right_down and right_paddle_y<=screen_height-paddle_height-paddle_movement_height:
+        right_paddle_y+=paddle_movement_height
 
 def handle_key_press(key_pressed):
     global left_down, left_up, right_down, right_up, screen_height, left_paddle_y, right_paddle_y
 
     if key_pressed == "a":
-        print("move up left")
         left_up = True
-        left_down = False
     
     if key_pressed == "z":
         left_down = True
-        print("move down left", left_down, left_paddle_y)
-        left_up = False
 
     if key_pressed == "k":
         right_up = True
-        print("move up right")
-        right_down = False
     
     if key_pressed == "m":
-        right_up = False
-        print("move down right")
         right_down = True
 
+    if key_pressed == "q":
+        cs1_quit()
 
-start_graphics(main_draw, width=screen_width, height=screen_height, key_press=handle_key_press)
+def handle_key_release(key_released):
+
+    global left_down, left_up, right_down, right_up
+
+    if key_released == "a":
+        left_up = False
+    
+    if key_released == "z":
+        left_down = False
+
+    if key_released == "k":
+        right_up = False
+    
+    if key_released == "m":
+        right_down = False
+
+start_graphics(main_draw, width=screen_width, height=screen_height, key_press=handle_key_press, key_release=handle_key_release)
